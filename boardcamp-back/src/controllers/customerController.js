aead42a9d29800311b690dbeca7a3647f8ee2bcb
Flávia Bulad <flavia.bulad@gmail.com>
2022-07-24 18:38:24 -0300

    feat: get customer by id and update customer

--- boardcamp-back/src/controllers/customerController.js
+++ boardcamp-back/src/controllers/customerController.js
@@ -50,6 +50,56 @@ export async function createCustomer(req, res) {
   }
 }
 
-export async function getCustomerById(req, res) {}
+export async function getCustomerById(req, res) {
+  const { id } = req.params;
 
-export async function updateCustomer(req, res) {}
+  try {
+    const customer = await connection.query(
+      `SELECT * FROM customers WHERE id = $1`,
+      [id]
+    );
+
+    if (customer.rowCount === 0) {
+      return res.status(404).send("Cliente não encontrado");
+    }
+    res.send(customer.rows[0]);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
+
+export async function updateCustomer(req, res) {
+  const customer = req.body;
+  const { id } = req.params;
+
+  try {
+    const search = await connection.query(
+      `
+    SELECT id FROM customers WHERE cpf = $1 AND id != $2
+  `,
+      [customer.cpf, id]
+    );
+    if (search.rowCount > 0) {
+      return res.sendStatus(409);
+    }
+
+    await connection.query(
+      `
+    UPDATE customers 
+    SET 
+      name = $1, 
+      phone = $2, 
+      cpf = $3, 
+      birthday = $4 
+    WHERE id = $5
+  `,
+      [customer.name, customer.phone, customer.cpf, customer.birthday, id]
+    );
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
