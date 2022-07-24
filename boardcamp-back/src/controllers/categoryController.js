e8e3cadd098609919b5059c57bb66c728a500e23
Flávia Bulad <flavia.bulad@gmail.com>
2022-07-24 12:57:19 -0300

    feat: category schema and validation

--- boardcamp-back/src/controllers/categoryController.js
+++ boardcamp-back/src/controllers/categoryController.js
@@ -1,3 +1,4 @@
+import { func } from "joi";
 import connection from "../dbStrategy/postgres.js";
 
 export default async function getCategories(req, res) {
@@ -8,4 +9,10 @@ export default async function getCategories(req, res) {
     console.log(error);
     res.status(500).send("Não foi possível pegar as categorias");
   }
+}
+
+export default async function createCategory(req, res){
+  const category = req.body;
+  
+
 }
