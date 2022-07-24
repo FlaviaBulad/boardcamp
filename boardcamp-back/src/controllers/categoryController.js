36d380b0e893698bc1c55e9438a6822ed7624160
Flávia Bulad <flavia.bulad@gmail.com>
2022-07-24 12:36:06 -0300

    feat: get categories controller and route

---
+++ boardcamp-back/src/controllers/categoryController.js
@@ -0,0 +1,11 @@
+import connection from "../dbStrategy/postgres.js";
+
+export default async function getCategories(req, res) {
+  try {
+    const { rows: categories } = connection.query(`SELECT * FROM categories`);
+    res.send(categories);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Não foi possível pegar as categorias");
+  }
+}
